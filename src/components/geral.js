2dac2bf05010ce72a43666af107c1e0fc877e0db
VitorFCM <vitor.carreramiguel@gmail.com>
2021-03-13 19:09:52 -0300

    Open file function added

---
+++ src/components/geral.js
@@ -0,0 +1,121 @@
+import './geral.css';
+
+import React, { useState } from 'react';
+
+
+const fs = window.require('fs');
+const {dialog} = window.require('electron').remote;
+
+class Conta{
+    constructor(nome, data){
+        this.nome = nome;
+        this.data = data;
+    }
+}
+
+class CsvParser{
+    constructor(fileContent){
+        var rows = fileContent.split('\n')
+
+        this.headers = rows[0].split(',')
+
+        this.accounts = [];
+        var i;
+        for (i = 1; i < rows.length; i++) {
+            var columns = rows[i].split(',')
+            
+            var newAccount = new Conta(columns[0], columns[1]);
+            
+            this.accounts.push(newAccount)
+        }
+    }
+}
+
+function dataInserction(params) {
+
+    var nameValue = document.getElementById("nameInput").value;
+    var dataValue = document.getElementById("dataInput").value;
+    
+    var novaConta = new Conta(nameValue, dataValue);
+
+    fs.readFile('./contas.json', 'utf8', function (err, data) {
+        if (err) {
+            console.log(err)
+        } else {
+            const file = JSON.parse(data);
+            
+            file.contas.push(novaConta);
+            
+            console.log(file.contas);
+
+            const json = JSON.stringify(file);
+            console.log(json);
+            fs.writeFile('./contas.json', json, 'utf8', function(err){
+                if(err){ 
+                  console.log(err); 
+                 } else {
+                  //Everything went OK!
+            }});
+        }
+    });
+
+}
+
+
+
+function openFile(){
+
+    let fileName = dialog.showOpenDialogSync();
+    
+    var data = fs.readFileSync(String(fileName), 'utf8');
+    var parsed = new CsvParser(data);
+    
+    return parsed;
+}
+
+function Geral(){
+    const [list, setList] = useState('');
+    
+    let headers, accounts
+    if(list !== undefined && list.headers !== undefined && list.account !== undefined){
+        headers = list.headers.map((header) =>
+            <td>{header}</td>
+        );
+
+        accounts = list.accounts.map((account) =>
+            <tr>
+                <td>{account.nome}</td>
+                <td>{account.data}</td>
+            </tr>
+        );
+    }
+    
+    
+    return (
+        <>  
+            
+            <table>
+                <tr>
+                    {headers === undefined ? null : headers}
+                </tr>
+                
+                {accounts === undefined ? null : accounts}
+                
+            </table>
+
+            <input type="text" name="name" id="nameInput"  placeholder="Nome"/>
+            <input type="text" name="data" id="dataInput"  placeholder="Data"/>
+            <button id="botao" onClick={dataInserction}>Clica</button>
+            <input type="button" id="openFile" onClick={()=>{
+                console.log("lista antes " + list)
+                let a = openFile();
+                console.log("clicou " + a)
+                setList(a);
+                console.log("lista depois " + list)
+            }} value="clica fdp"/> 
+            
+        </>
+    );
+}
+
+export default Geral;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import Geral from './components/Geral.js';
+import Geral from './components/geral.js';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render( 
@@ -12,7 +12,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
+ 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
